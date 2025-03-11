66060a06160c9f4e84163588381cf03252b02210
Foroutan <foroutan.dev@gmail.com>
2025-03-11 18:34:46 +0330

    feat: add serializers for Category and Tag models in taxonomy app

---
+++ taxonomy/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Category, Tag
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name', 'slug']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name', 'slug']
