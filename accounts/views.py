4e04db98f6c9fca1e16d9beeab7db70764e04487
Foroutan <foroutan.dev@gmail.com>
2025-03-21 14:38:32 +0330

    refactor: remove unused import

--- accounts/views.py
+++ accounts/views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
-from rest_framework_simplejwt.tokens import RefreshToken
 
 from .models import OTP, MyUser
 from .serializers import MyUserSerializer
